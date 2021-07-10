a6eb6c5248941bad2cbd2a798cafecf8dd6c7366
asadiqbalkhan <iq.asad@gmail.com>
2021-07-10 11:38:09 +0500

    ropsten deployment done

--- truffle-config.js
+++ truffle-config.js
@@ -59,13 +59,12 @@ module.exports = {
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
     ropsten: {
-      provider: () =>
-        new HDWalletProvider(
-          mnemonic,
-          `https://ropsten.infura.io/v3/${infuraKey}}`
-        ),
+      provider: new HDWalletProvider(
+        mnemonic,
+        `https://ropsten.infura.io/v3/${infuraKey}`
+      ),
       network_id: 3, // Ropsten's id
-      gas: 5500000, // Ropsten has a lower block limit than mainnet
+      gas: 7000000, // Ropsten has a lower block limit than mainnet
       confirmations: 2, // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
@@ -91,6 +90,7 @@ module.exports = {
         optimizer: {
           enabled: false,
           runs: 200,
+          version: "^0.5.0",
         },
         evmVersion: "byzantium",
       },
